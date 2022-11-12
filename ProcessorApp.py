5c66695ea74330caef5585db83929227ea8ba5ec
Artem <artem.alshan@yandex.ru>
2022-11-12 22:45:04 +0300

    ProcessorApp.py

--- ProcessorApp.py
+++ ProcessorApp.py
@@ -4,33 +4,57 @@ from youtube_dl import YoutubeDL
 
 class Processor(QThread):
     chunks = pyqtSignal(int)
-    info_video = pyqtSignal(str, str, str)
+    info_video = pyqtSignal(str, str, str, str)
 
-    def __init__(self, url, path):
-        super().__init__()
+    def __init__(self, url, path, check_status, type_file):
+        super(Processor, self).__init__()
 
         self.url = url
         self.path = path
+        self.check_status = check_status
+        self.type_file = type_file
 
-    def run(self):
+    def run(self) -> None:
+        type_video = self.type_file['video']
         try:
+            if self.check_status == 0:
+                with YoutubeDL({'quiet': True}) as ydl:
+                    meta = ydl.extract_info(self.url, download=False)
+                    video_title = meta.get('title', None)
+                    video_description = meta.get('description', None)
+                    video_thumbnails = meta['thumbnails'][3]['url']
 
-            with YoutubeDL({'quiet': True}) as ydl:
-                meta = ydl.extract_info(self.url, download=False)
-                video_title = meta.get('title', None)
-                video_description = meta.get('description', None)
-                video_thumbnails = meta['thumbnails'][3]['url']
+                    self.info_video.emit(video_title, video_description, video_thumbnails, type_video)
 
-                self.info_video.emit(video_title, video_description, video_thumbnails)
+                    dl_options = {
+                        'format': 'best',
+                        'outtmpl': self.path + "/" + video_title + ".mp4",
+                        'progress_hooks': [self.progress]
+                    }
 
-                dl_options = {
-                    'format': 'best',
-                    'outtmpl': self.path + "/" + video_title + ".mp4",
-                    'progress_hooks': [self.progress]
-                }
+                    with YoutubeDL(dl_options) as dl:
+                        dl.download([self.url])
+            else:
+                type_audio = self.type_file['audio']
+                with YoutubeDL({'quiet': True}) as ydl:
+                    meta = ydl.extract_info(self.url, download=False)
+                    video_title = meta.get('title', None)
+                    video_description = meta.get('description', None)
+                    video_thumbnails = meta['thumbnails'][3]['url']
 
-                with YoutubeDL(dl_options) as dl:
-                    dl.download([self.url])
+                    self.info_video.emit(video_title, video_description, video_thumbnails, type_audio)
+
+                    audio = YoutubeDL({'format': 'bestaudio',
+                                       'postprocessors': [{
+                                           'key': 'FFmpegExtractAudio',
+                                           'preferredcodec': 'mp3',
+                                           'preferredquality': '192',
+                                       }],
+                                       'outtmpl': self.path + "/" + video_title + ".mp4",
+                                       'progress_hooks': [self.progress]
+                                       })
+                    audio.extract_info(self.url)
+
         except Exception as ex:
             print(ex)
 
@@ -40,5 +64,3 @@ class Processor(QThread):
 
             print(round(percent['downloaded_bytes'] / percent['total_bytes'] * 100, 1))
             self.chunks.emit(int(result))
-
-
